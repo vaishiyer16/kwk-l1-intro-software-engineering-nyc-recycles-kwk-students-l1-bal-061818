66587886293d21cfb0ab3366d1d41391fdb2cd75
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-21 18:33:45 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -47,6 +47,10 @@ end
 
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
+def borough(hash)
+  hash["borough"]
+end
+
 
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash.
